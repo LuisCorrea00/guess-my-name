bf45c7863fbae1de16634f06760a53e109fa76ed
Luis Correa <henriqueluis785@gmail.com>
2023-03-09 22:46:56 -0300

    language update

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const displayMessage = function (message) {
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    displayMessage('No number!');
+    displayMessage('Nenhum número!');
 
   } else if (guess === secretNumber) {
-      displayMessage('Correct number!');
+      displayMessage('Número Correto!');
       document.querySelector('.number').textContent = secretNumber;
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
@@ -25,12 +25,12 @@ document.querySelector('.check').addEventListener('click', function () {
       }
 
   } else if (guess !== secretNumber && score > 1) {
-    displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+    displayMessage(guess > secretNumber ? 'Muito alto!' : 'Muito baixo!');
     score--;
     document.querySelector('.score').textContent = score;
 
   }else if (score <= 1) {
-      displayMessage('You lost the game!');
+      displayMessage('Você perdeu!');
       score--;
       document.querySelector('.score').textContent = score;
   }
@@ -40,7 +40,7 @@ document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
   document.querySelector('.score').textContent = score;
-  displayMessage('Start guessing...');
+  displayMessage('Comece a adivinhar...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
